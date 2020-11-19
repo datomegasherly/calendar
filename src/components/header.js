db9376f4a8bccde5c508c5ea89f768a7380340a3
datomegasherly <dat.omega.sherly@gmail.com>
2020-11-19 17:48:23 +0330

    change Header from component to function and use useContext

--- src/components/header.js
+++ src/components/header.js
@@ -1,20 +1,15 @@
-import React, { Component } from 'react';
+import React, { useContext } from 'react';
 import MainContext from '../context';
 
-class Header extends Component {
-    static contextType = MainContext;
-    constructor(props){
-        super(props);
-    }
-    render() {
-        let { test, setTest } = this.context;
-        return (
-            <div>
-                {test}
-                <button onClick={() => setTest('name')}>TTT</button>
-            </div>
-        )
-    }
+function Header() {
+    let context = useContext(MainContext);
+    let { test, setTest } = context;
+    return (
+        <div>
+            {test}
+            <button onClick={() => setTest('name')}>TTT</button>
+        </div>
+    )
 }
 
 export default Header;
