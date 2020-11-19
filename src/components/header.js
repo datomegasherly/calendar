57414886305bd66c55b3a902bfd69cb95280736f
datomegasherly <dat.omega.sherly@gmail.com>
2020-11-20 00:51:59 +0330

    add year to header and move Year component to helper

--- src/components/header.js
+++ src/components/header.js
@@ -1,14 +1,28 @@
 import React, { useContext } from 'react';
 import MainContext from '../context';
+import { Grid, Button, FormControl, Select, MenuItem, FormHelperText, makeStyles } from '@material-ui/core';
+import { Year } from '../helper';
 
 function Header() {
     let context = useContext(MainContext);
-    let { test, setTest } = context;
+    let { year } = context;
     return (
-        <div>
-            {test}
-            <button onClick={() => setTest('name')}>TTT</button>
-        </div>
+        <Grid container>
+            <Grid item container xs={6}>
+                <Grid item xs={12} sm={12} md={4}>
+                    <Year />
+                </Grid>
+                <Grid item xs={12} sm={12} md={4}>
+                    
+                </Grid>
+                <Grid item xs={12} sm={12} md={4}>
+                    
+                </Grid>
+            </Grid>
+            <Grid item xs={6}>
+                {year}
+            </Grid>
+        </Grid>
     )
 }
 
