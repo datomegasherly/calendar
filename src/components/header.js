2cd0e1c7a77f55c93b47b9cfacef413c96956837
datomegasherly <dat.omega.sherly@gmail.com>
2020-11-21 16:37:50 +0330

    create Add button to create event in selected date

--- src/components/header.js
+++ src/components/header.js
@@ -1,6 +1,8 @@
 import React, { useContext } from 'react';
 import MainContext from '../context';
-import { Grid, Button, Box } from '@material-ui/core';
+import { Grid, Button, Box, Hidden } from '@material-ui/core';
+import AddIcon from '@material-ui/icons/Add';
+import TodayIcon from '@material-ui/icons/Today';
 import { useStyles, Year, Month, Day, getDate } from '../helper';
 
 function updateDate({setYear, setMonth, setDay}) {
@@ -16,7 +18,7 @@ function Header() {
     let classes = useStyles();
     return (
         <Grid container>
-            <Grid item container xs={9}>
+            <Grid item container xs={8} sm={9} md={9} lg={10}>
                 <Grid className={classes.padding} item xs={4}>
                     <Year />
                 </Grid>
@@ -27,9 +29,18 @@ function Header() {
                     <Day />
                 </Grid>
             </Grid>
-            <Box xs={6} mt={1} ml={2}>
-                <Button onClick={() => updateDate({setYear, setMonth, setDay})} variant="contained" color="primary">Today</Button>
-            </Box>
+            <Grid item container xs={4} sm={3} md={3} lg={2} pl={2}>
+                <Box mt={1} width="50%" align="center">
+                    <Button onClick={() => updateDate({setYear, setMonth, setDay})} variant="outlined" color="primary">
+                        <TodayIcon /><Hidden only={['xs', 'sm']}> Today</Hidden>
+                    </Button>
+                </Box>
+                <Box mt={1} width="50%" align="center">
+                    <Button onClick={() => updateDate({setYear, setMonth, setDay})} variant="outlined" color="primary">
+                        <AddIcon /><Hidden only={['xs', 'sm']}> Add</Hidden>
+                    </Button>
+                </Box>
+            </Grid>
         </Grid>
     )
 }
