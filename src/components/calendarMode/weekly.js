d20570788a5839f9fb5d38a6273f6d69accfece9
datomegasherly <dat.omega.sherly@gmail.com>
2020-11-22 15:17:23 +0330

    add weekly mode

---
+++ src/components/calendarMode/weekly.js
@@ -0,0 +1,96 @@
+import React, { useContext } from 'react';
+import MainContext from '../../context';
+import { getLastDay, getCurrentDay, Days, getDate, useStyles } from '../../helper';
+import { Grid, Paper, Typography, Hidden } from '@material-ui/core';
+import classNames from 'classnames';
+
+function Weekly() {
+    const context = useContext(MainContext);
+    let { year, month, day, setDay, events } = context;
+    let currentDate = getDate();
+    let lastDay = getLastDay(year, month);
+    let days = [];
+    for(let i = 1;i <= lastDay;i++){
+        days.push(i);
+    }
+    let classes = useStyles();
+    let currentWeekDays = []; // list of current week days
+    let beforeDays = [];
+    let afterDays = [];
+    let currentDay = getCurrentDay(year, month, day);
+    for(let i = day - currentDay;i <= day + (6 - currentDay);i++){
+        if(i <= 0){
+            beforeDays.push(i);
+        } else if(i > lastDay) {
+            afterDays.push(i);
+        } else {
+            currentWeekDays.push(i);
+        }
+    }
+    return (
+        <Grid style={{marginTop: '15px'}} container>
+            {
+                Days.map(ds => {
+                    return (
+                        <Grid key={Math.random()} className={classNames(classes.boxMargin, classes.boxSize)} item>
+                            <Hidden only={['xs', 'sm', 'md']}>
+                                <Typography variant="h6" align="center">{ds}</Typography>
+                            </Hidden>
+                            <Hidden only={['lg', 'xl']}>
+                                <Typography variant="h6" align="center">{ds.toString().slice(0,3)}</Typography>
+                            </Hidden>
+                        </Grid>
+                    )
+                })
+            }
+            {
+                beforeDays.map(sd => {
+                    return (
+                        <Grid key={Math.random()} className={classNames(classes.boxMargin, classes.boxSize)} item>
+
+                        </Grid>
+                    )
+                })
+            }
+            {
+                currentWeekDays.map(d => {
+                    return (
+                        <Grid key={d} className={classNames(classes.boxMargin, classes.boxSize)} item>
+                            <Paper elevation={2} className={classNames(classes.cardBox, 
+                                year == currentDate[0] && 
+                                month == currentDate[1] && 
+                                d == currentDate[2] ? 'current' : d == day ? 'selected' : '')}
+
+                                onClick={() => setDay(d)}
+                            >
+                                <Typography variant="h6">{d}</Typography>
+                                <Typography className={classes.smallText}>
+                                    {Days[getCurrentDay(year, month, d)]}
+                                </Typography>
+                                <Typography>
+                                    { 
+                                        events && 
+                                        events[`${year}-${month}-${d}`] && 
+                                        events[`${year}-${month}-${d}`].length ?  
+                                            `${events[`${year}-${month}-${d}`].length} Events` : 'No Events'
+                                    }
+                                </Typography>
+                            </Paper>
+                        </Grid>
+                    )
+                })
+            }
+            {
+                afterDays.map(sd => {
+                    return (
+                        <Grid key={Math.random()} className={classNames(classes.boxMargin, classes.boxSize)} item>
+
+                        </Grid>
+                    )
+                })
+            }
+        </Grid>
+    )
+}
+
+export default Weekly;
