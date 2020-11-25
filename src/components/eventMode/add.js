670008ded03e8cc14cf4a544aedc6101c16014ce
datomegasherly <dat.omega.sherly@gmail.com>
2020-11-25 09:56:38 +0330

    finalize saveEvent function to save current event of selected day

--- src/components/eventMode/add.js
+++ src/components/eventMode/add.js
@@ -1,18 +1,44 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { useStyles, theme } from '../../helper';
+import { v4 } from 'uuid';
 import { Link } from 'react-router-dom';
 import { Button, FormControl, Grid, Input, InputLabel } from '@material-ui/core';
 import { ThemeProvider } from '@material-ui/styles';
 import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
 import { TimePicker, MuiPickersUtilsProvider } from '@material-ui/pickers';
 import DateFnsUtils from '@date-io/date-fns';
+import MainContext from '../../context';
 
 function Add(props){
-    let { event, handleEventChange, handleTimeChange, checkEvent } = props;
+    let { event, setEvent, handleEventChange, handleTimeChange, checkEvent } = props;
+    let context = useContext(MainContext);
+    let { year, month, day, events, setEvents, setMode } = context;
     let classes = useStyles();
-    let saveEvent = () => {
+    /**
+     * use async/await function because setEvents ( in context ) will reset eventMode function and it will remove state
+     */
+    let saveEvent = async() => {
         if(checkEvent()){
-            // save event
+            let currentEvent = events[`${year}-${month}-${day}`];
+            if(!currentEvent){
+                currentEvent = [];
+            }
+            let start_time = {hour: event.startTime.getHours(), minute: event.startTime.getMinutes()};
+            let end_time = {hour: event.endTime.getHours(), minute: event.endTime.getMinutes()};
+            let full = `${year}-${month < 10 ? `0${month}` : month}-${day < 10 ? `0${day}` : day}`
+            currentEvent.push({
+                id: v4(),
+                year,
+                month,
+                day,
+                full,
+                event: event.title,
+                start_time,
+                end_time
+            });
+            await setEvent({...event, redirect: true});
+            setEvents({...events, [`${year}-${month}-${day}`]: currentEvent}); // add event to selected day
+            setMode(1); // change mode to daily
         }
     }
     return (
@@ -33,6 +59,7 @@ function Add(props){
                             </Button>
                         </Grid>
                     </Grid>
+                    <Grid container item xs={12}>Current Date : {`${year}/${month}/${day}`}</Grid>
                     <Grid container item xs={12} sm={12} md={6}>
                         <Grid item mt={5} xs={12} className={classes.padding}>
                             <FormControl className={classes.selectSize}>
