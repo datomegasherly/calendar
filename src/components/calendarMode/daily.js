e29c1131986d04117ea61f6c9e421f6d62fb924f
datomegasherly <dat.omega.sherly@gmail.com>
2020-11-26 20:46:40 +0330

    add base timeline to daily calendar

--- src/components/calendarMode/daily.js
+++ src/components/calendarMode/daily.js
@@ -3,6 +3,15 @@ import MainContext from '../../context';
 import { getLastDay, getCurrentDay, Days, getDate, useStyles } from '../../helper';
 import { Grid, Paper, Typography, Hidden } from '@material-ui/core';
 import classNames from 'classnames';
+import { 
+    Timeline, 
+    TimelineConnector, 
+    TimelineItem, 
+    TimelineOppositeContent, 
+    TimelineSeparator, 
+    TimelineDot,
+    TimelineContent } from '@material-ui/lab';
+import FastfoodIcon from '@material-ui/icons/Fastfood';
 
 function Monthly() {
     const context = useContext(MainContext);
@@ -12,12 +21,10 @@ function Monthly() {
     return (
         <Grid style={{marginTop: '15px'}} container>
             <Grid className={classNames(classes.boxMargin, classes.selectSize)} item>
-                <Paper elevation={2} className={classNames(classes.cardBox, 
-                    year == currentDate[0] && 
+                <Paper elevation={2} className={classes.cardBox}>
+                    <Typography variant="h6">{day}{year == currentDate[0] && 
                     month == currentDate[1] && 
-                    day == currentDate[2] ? 'current' : '')}
-                >
-                    <Typography variant="h6">{day}</Typography>
+                    day == currentDate[2] ? ' ( Today )' : ''}</Typography>
                     <Typography className={classes.smallText}>
                         {Days[getCurrentDay(year, month, day)]}
                     </Typography>
@@ -26,7 +33,30 @@ function Monthly() {
                             events && 
                             events[`${year}-${month}-${day}`] && 
                             events[`${year}-${month}-${day}`].length ?  
-                                `${events[`${year}-${month}-${day}`].length} Events` : 'No Events'
+                            <Timeline align="alternate">
+                                <TimelineItem>
+                                    <TimelineOppositeContent>
+                                        <Typography variant="body2" color="textSecondary">
+                                            9:30 am
+                                        </Typography>
+                                    </TimelineOppositeContent>
+                                    <TimelineSeparator>
+                                        <TimelineDot>
+                                            <FastfoodIcon />
+                                        </TimelineDot>
+                                        <TimelineConnector />
+                                    </TimelineSeparator>
+                                    <TimelineContent>
+                                        <Paper elevation={3} className={classes.paper}>
+                                            <Typography variant="h6" component="h1">
+                                            Eat
+                                            </Typography>
+                                            <Typography>Because you need strength</Typography>
+                                        </Paper>
+                                    </TimelineContent>
+                                </TimelineItem>
+                            </Timeline>
+                            : 'No Events'
                         }
                     </Typography>
                 </Paper>
