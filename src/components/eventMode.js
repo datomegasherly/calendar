9406174f00f9c546594a998ca0a5de77db306673
datomegasherly <dat.omega.sherly@gmail.com>
2020-12-01 21:23:59 +0330

    add some features for event mode in edit mode

--- src/components/eventMode.js
+++ src/components/eventMode.js
@@ -1,25 +1,31 @@
 import { Grid, Snackbar } from '@material-ui/core';
-import React, { useState, useEffect, Fragment } from 'react';
+import React, { useState, useEffect, useContext, Fragment } from 'react';
 import MuiAlert from '@material-ui/lab/Alert';
 import Add from './eventMode/add';
+import Edit from './eventMode/edit';
 import { Redirect } from 'react-router-dom';
 import Joi from 'joi';
 import { SpinnerComponent } from 'react-element-spinner';
+import MainContext from '../context';
+import { numToStr } from '../helper';
 
 function Alert(props) {
     return <MuiAlert elevation={6} variant="filled" {...props} />;
 }
 
-function EventMode(propType){
+function EventMode(propType, prop){
     let [ event, setEvent ] = useState({
+        id: false,
         title: '',
         startTime: '',
         endTime: '',
         error: false,
         errorTitle: '',
         redirect: false,
-        loading: false
+        loading: false,
+        dataIsLoaded: false // this variable use in edit mode and will ignore useEffect call when dataIsLoaded is true
     });
+    let { events, year, month, day } = useContext(MainContext);
     /**
      * Update current state once ( like ComponentDidUpdate )
      */
@@ -27,8 +33,44 @@ function EventMode(propType){
         let full = `1900-01-01`;
         let startTime = new Date(`${full}T00:00:00`);
         let endTime = new Date(`${full}T00:00:00`);
-        setEvent({...event, startTime, endTime});
+        if(propType === 'create'){
+            setEvent({...event, startTime, endTime, dataIsLoaded: true});
+        }
     }, []); // when use [] after useEffect , it will call once
+
+    /**
+     * waiting for loading data when use edit link directly
+     * if id does not exist , it will redirect to home page
+     */
+    useEffect(() => {
+        if(propType === 'edit' && prop.match.params.id && !event.dataIsLoaded){
+            let newDay = numToStr(day);
+            let newMonth = numToStr(month);
+            let id = prop.match.params.id;
+            let full = `${year}-${newMonth}-${newDay}`;
+            if(events && events[full]){
+                let data = events[full].find(r => r.id == id);
+                if(data){
+                    let startTime = new Date(`${full}T${numToStr(data.start_time.hour)}:${numToStr(data.start_time.minute)}:00`);
+                    let endTime = new Date(`${full}T${numToStr(data.end_time.hour)}:${numToStr(data.end_time.minute)}:00`);
+                    setEvent({
+                        id: prop.match.params.id,
+                        startTime,
+                        endTime,
+                        title: data.event,
+                        dataIsLoaded: true,
+                        loading: false
+                    });
+                } else {
+                    setEvent({redirect: true, dataIsLoaded: true});
+                    return;
+                }
+            } else {
+                setEvent({loading: true});
+                return;
+            }
+        }
+    });
 
     const checkEvent = (type = 'all', data = false) => {
         let ev = {title: event.title, startTime: event.startTime, endTime: event.endTime};
@@ -89,6 +131,9 @@ function EventMode(propType){
             </Snackbar>
             {
                 propType == 'add' ? <Add {...props} /> : ''
+            }
+            {
+                propType == 'edit' ? <Edit {...props} /> : ''
             }
         </Fragment>
     )
