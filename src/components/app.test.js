8937c521c279f70ed9b36456ec9e98c5b534da33
datomegasherly <dat.omega.sherly@gmail.com>
2020-12-05 22:58:06 +0330

    some try to test useEffect in jest

--- src/components/app.test.js
+++ src/components/app.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import App from './app';
 import { getByAttr } from '../../test/testUtil';
+import { getDate } from '../helper';
 
 const setup = () => {
     return shallow(<App />);
@@ -12,4 +13,14 @@ describe('App Component :', () => {
         let wrapper = setup();
         let Component = getByAttr(wrapper, 'app-component');
     });
-})
+});
+
+describe('state control of useEffect', () => {
+    test('update year, month and day when useEffect call', () => {
+        const mockSetState = jest.fn();
+        //let date = getDate();
+        React.useState = jest.fn(() => ["", mockSetState]);
+        jest.spyOn(React, 'useEffect').mockImplementation(f => jest.fn());
+        const wrapper = setup();
+    });
+});
