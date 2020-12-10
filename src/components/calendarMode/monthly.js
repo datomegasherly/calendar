8b5eb45836521a5f16706bdfd7483144eda6556e
datomegasherly <dat.omega.sherly@gmail.com>
2020-12-10 23:00:50 +0330

    update weekly and monthly because of moving to base file

--- src/components/calendarMode/monthly.js
+++ src/components/calendarMode/monthly.js
@@ -1,85 +1,24 @@
 import React, { useContext } from 'react';
 import MainContext from '../../context';
-import { getLastDay, getCurrentDay, Days, getDate, useStyles, numToStr } from '../../helper';
-import { Grid, Paper, Typography, Hidden } from '@material-ui/core';
-import classNames from 'classnames';
+import { getLastDay, getCurrentDay, Days } from '../../helper';
+import { Grid } from '@material-ui/core';
+import { dayBox } from './base';
 
 function Monthly() {
     const context = useContext(MainContext);
-    let { year, month, day, events } = context.state;
-    let { setYear, setMonth, setDay, setMode } = context.dispatch;
-    let changeState = (...date) => {
-        setYear(date[0]);
-        setMonth(date[1]);
-        setDay(date[2]);
-        setMode(1);
-    }
-    let currentDate = getDate();
+    let { year, month } = context.state;
     let lastDay = getLastDay(year, month);
     let days = [];
     for(let i = 1;i <= lastDay;i++){
         days.push(i);
     }
-    let classes = useStyles();
     let startDay = [];
     for(let i = 0;i <= getCurrentDay(year, month, 1)-1;i++){
         startDay.push(i);
     }
     return (
         <Grid style={{marginTop: '15px'}} container>
-            {
-                Days.map(ds => {
-                    return (
-                        <Grid key={Math.random()} className={classNames(classes.boxMargin, classes.boxSize)} item>
-                            <Hidden only={['xs', 'sm', 'md']}>
-                                <Typography variant="h6" align="center">{ds}</Typography>
-                            </Hidden>
-                            <Hidden only={['lg', 'xl']}>
-                                <Typography variant="h6" align="center">{ds.toString().slice(0,3)}</Typography>
-                            </Hidden>
-                        </Grid>
-                    )
-                })
-            }
-            {
-                startDay.map(sd => {
-                    return (
-                        <Grid key={Math.random()} className={classNames(classes.boxMargin, classes.boxSize)} item>
-
-                        </Grid>
-                    )
-                })
-            }
-            {
-                days.map(d => {
-                    let newDay = numToStr(d);
-                    let newMonth = numToStr(month);
-                    return (
-                        <Grid key={d} className={classNames(classes.boxMargin, classes.boxSize)} item>
-                            <Paper elevation={2} className={classNames(classes.cardBox, 
-                                year == currentDate[0] && 
-                                month == currentDate[1] && 
-                                d == currentDate[2] ? 'current' : d == day ? 'selected' : '')}
-
-                                onClick={() => setDay(d)}
-                            >
-                                <Typography variant="h6">{d}</Typography>
-                                <Typography className={classes.smallText}>
-                                    {Days[getCurrentDay(year, month, d)]}
-                                </Typography>
-                                <Typography>
-                                    { 
-                                        events && 
-                                        events[`${year}-${newMonth}-${newDay}`] && 
-                                        events[`${year}-${newMonth}-${newDay}`].length ?  
-                                            <Grid onClick={() => changeState(year,month,d)}>{events[`${year}-${newMonth}-${newDay}`].length} Events</Grid> : 'No Events'
-                                    }
-                                </Typography>
-                            </Paper>
-                        </Grid>
-                    )
-                })
-            }
+            { dayBox({startDay, endDay: [], days, Days}) }
         </Grid>
     )
 }
--- src/components/calendarMode/weekly.js
+++ src/components/calendarMode/weekly.js
@@ -1,20 +1,17 @@
 import React, { useContext } from 'react';
 import MainContext from '../../context';
-import { getLastDay, getCurrentDay, Days, getDate, useStyles, numToStr } from '../../helper';
-import { Grid, Paper, Typography, Hidden } from '@material-ui/core';
-import classNames from 'classnames';
+import { getLastDay, getCurrentDay, Days, getDate, useStyles } from '../../helper';
+import { Grid } from '@material-ui/core';
+import { dayBox } from './base';
 
 function Weekly() {
     const context = useContext(MainContext);
-    let { year, month, day, events } = context.state;
-    let { setDay } = context.dispatch;
-    let currentDate = getDate();
+    let { year, month, day } = context.state;
     let lastDay = getLastDay(year, month);
     let days = [];
     for(let i = 1;i <= lastDay;i++){
         days.push(i);
     }
-    let classes = useStyles();
     let currentWeekDays = []; // list of current week days
     let beforeDays = [];
     let afterDays = [];
@@ -30,68 +27,7 @@ function Weekly() {
     }
     return (
         <Grid style={{marginTop: '15px'}} container>
-            {
-                Days.map(ds => {
-                    return (
-                        <Grid key={Math.random()} className={classNames(classes.boxMargin, classes.boxSize)} item>
-                            <Hidden only={['xs', 'sm', 'md']}>
-                                <Typography variant="h6" align="center">{ds}</Typography>
-                            </Hidden>
-                            <Hidden only={['lg', 'xl']}>
-                                <Typography variant="h6" align="center">{ds.toString().slice(0,3)}</Typography>
-                            </Hidden>
-                        </Grid>
-                    )
-                })
-            }
-            {
-                beforeDays.map(sd => {
-                    return (
-                        <Grid key={Math.random()} className={classNames(classes.boxMargin, classes.boxSize)} item>
-
-                        </Grid>
-                    )
-                })
-            }
-            {
-                currentWeekDays.map(d => {
-                    let newDay = numToStr(d);
-                    let newMonth = numToStr(month);
-                    return (
-                        <Grid key={d} className={classNames(classes.boxMargin, classes.boxSize)} item>
-                            <Paper elevation={2} className={classNames(classes.cardBox, 
-                                year == currentDate[0] && 
-                                month == currentDate[1] && 
-                                d == currentDate[2] ? 'current' : d == day ? 'selected' : '')}
-
-                                onClick={() => setDay(d)}
-                            >
-                                <Typography variant="h6">{d}</Typography>
-                                <Typography className={classes.smallText}>
-                                    {Days[getCurrentDay(year, month, d)]}
-                                </Typography>
-                                <Typography>
-                                    { 
-                                        events && 
-                                        events[`${year}-${newMonth}-${newDay}`] && 
-                                        events[`${year}-${newMonth}-${newDay}`].length ?  
-                                            `${events[`${year}-${newMonth}-${newDay}`].length} Events` : 'No Events'
-                                    }
-                                </Typography>
-                            </Paper>
-                        </Grid>
-                    )
-                })
-            }
-            {
-                afterDays.map(sd => {
-                    return (
-                        <Grid key={Math.random()} className={classNames(classes.boxMargin, classes.boxSize)} item>
-
-                        </Grid>
-                    )
-                })
-            }
+            { dayBox({startDay: beforeDays, endDay: afterDays, days: currentWeekDays, Days}) }
         </Grid>
     )
 }
