c885c65b5bdfcef1eaed24c5d2230c2b40a1830d
datomegasherly <dat.omega.sherly@gmail.com>
2020-12-18 22:59:15 +0330

    some clean code in header

--- src/components/header.js
+++ src/components/header.js
@@ -14,35 +14,48 @@ function updateDate({setEvents, setYear, setMonth, setDay}) {
     updateData({setEvents}, year, month);
 }
 
-function Header() {
-    let context = useContext(MainContext);
-    let { year, month, day, mode } = context.state;
-    let { setEvents, setYear, setMonth, setDay } = context.dispatch;
+function FullDateInput(){
     let classes = useStyles();
+    return (
+        <React.Fragment>
+            <Grid className={classes.padding} item xs={4}>
+                <Year />
+            </Grid>
+            <Grid className={classes.padding} item xs={4}>
+                <Month />
+            </Grid>
+            <Grid className={classes.padding} item xs={4}>
+                <Day />
+            </Grid>
+        </React.Fragment>
+    )
+}
+
+function CenterButtonGroup(){
+    let context = useContext(MainContext);
+    let { setEvents, setYear, setMonth, setDay } = context.dispatch;
+    return (
+        <Box mt={1} width="33%" align="center">
+            <ButtonGroup variant="contained" color="primary" aria-label="contained primary button group">
+                <Button onClick={() => updateDate({setEvents, setYear, setMonth, setDay})}>
+                    <TodayIcon /><Hidden only={['xs', 'sm']}> Today</Hidden>
+                </Button>
+                <Button component={Link} to="/add" color="primary" aria-label="contained primary button">
+                    <AddIcon /><Hidden only={['xs', 'sm']}> Add</Hidden>
+                </Button>
+            </ButtonGroup>
+        </Box>
+    )
+}
+
+function Header() {
     return (
         <Grid container>
             <Grid item container xs={6} sm={8} md={5} lg={6}>
-                <Grid className={classes.padding} item xs={4}>
-                    <Year />
-                </Grid>
-                <Grid className={classes.padding} item xs={4}>
-                    <Month />
-                </Grid>
-                <Grid className={classes.padding} item xs={4}>
-                    <Day />
-                </Grid>
+                <FullDateInput />
             </Grid>
             <Grid item container xs={6} sm={4} md={3} lg={2} pl={2}>
-                <Box mt={1} width="33%" align="center">
-                    <ButtonGroup variant="contained" color="primary" aria-label="contained primary button group">
-                        <Button onClick={() => updateDate({setEvents, setYear, setMonth, setDay})}>
-                            <TodayIcon /><Hidden only={['xs', 'sm']}> Today</Hidden>
-                        </Button>
-                        <Button component={Link} to="/add" color="primary" aria-label="contained primary button">
-                            <AddIcon /><Hidden only={['xs', 'sm']}> Add</Hidden>
-                        </Button>
-                    </ButtonGroup>
-                </Box>
+                <CenterButtonGroup />
             </Grid>
             <Grid item container xs={12} sm={12} md={4} lg={3} pl={2}>
                 <Mode />
