3233398e26ae2193db9c0dd81a00eba5a361d87e
datomegasherly <dat.omega.sherly@gmail.com>
2020-12-27 17:24:37 +0330

    add dialog to add and edit mode instead of show in new window

--- src/components/eventMode/add.js
+++ src/components/eventMode/add.js
@@ -7,14 +7,19 @@ import { ThemeProvider } from '@material-ui/styles';
 import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
 import { TimePicker, MuiPickersUtilsProvider } from '@material-ui/pickers';
 import DateFnsUtils from '@date-io/date-fns';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
 import MainContext from '../../context';
 import axios from 'axios';
 
 function Add(props){
     let { event, setEvent, handleEventChange, handleTimeChange, checkEvent } = props;
     let context = useContext(MainContext);
-    let { year, month, day, events } = context.state;
-    let { setEvents, setMode } = context.dispatch;
+    let { year, month, day, events, open } = context.state;
+    let { setEvents, setMode, setOpen } = context.dispatch;
     let classes = useStyles();
     let saveEvent = () => {
         if(checkEvent()){
@@ -44,6 +49,7 @@ function Add(props){
                 if(res.data.success){
                     events.push(data);
                     await setEvent({...event, redirect: true});
+                    setOpen(false);
                     setEvents(events); // add event to selected day
                     setMode(1); // change mode to daily
                 } else if(res.data.error){
@@ -56,59 +62,78 @@ function Add(props){
             });
         }
     }
+    let handleClose = () => {
+        setOpen(false);
+    }
     return (
         <ThemeProvider theme={theme}>
-            <MuiPickersUtilsProvider utils={DateFnsUtils}>
-                <Grid container>
-                    <Grid container className={classes.marginBottom}>
-                        <Grid item xs={4} sm={4} md={6}>
-                            <Link to="/" className={classes.links}>
-                                <Button startIcon={<ArrowBackIosIcon />} variant="contained" color="secondary">
-                                    Back
+            <Dialog 
+                open={open}
+                onClose={handleClose}
+                aria-labelledby="alert-dialog-title"
+                aria-describedby="alert-dialog-description"
+                disableBackdropClick={true}
+                disableEscapeKeyDown={true}
+            >
+                <DialogTitle id="alert-dialog-title">Current Date : {`${year}/${month}/${day}`}</DialogTitle>
+                <DialogContent>
+                    <DialogContentText id="alert-dialog-description">
+                        
+                            <MuiPickersUtilsProvider utils={DateFnsUtils}>
+                                <Grid container>
+                                    <Grid container item xs={12} sm={12} md={6}>
+                                        <Grid item mt={5} xs={12} className={classes.padding}>
+                                            <FormControl className={classes.selectSize}>
+                                                <InputLabel htmlFor="event">Event Description</InputLabel>
+                                                <Input id="event" value={event.title || ''} onChange={handleEventChange} />
+                                            </FormControl>
+                                        </Grid>
+                                    </Grid>
+                                    <Grid container item xs={12} sm={12} md={6}>
+                                        <Grid item mt={5} xs={12} sm={6} className={classes.padding}>
+                                            <FormControl className={classes.selectSize}>
+                                                <TimePicker
+                                                    ampm={false}
+                                                    id="startTime"
+                                                    label="Start Time"
+                                                    value={event.startTime}
+                                                    onChange={(date) => handleTimeChange(date, 'start')}
+                                                />
+                                            </FormControl>
+                                        </Grid>
+                                        <Grid item xs={12} sm={6} className={classes.padding}>
+                                            <FormControl className={classes.selectSize}>
+                                                <TimePicker
+                                                    ampm={false}
+                                                    id="endTime"
+                                                    label="End Time"
+                                                    value={event.endTime}
+                                                    onChange={(date) => handleTimeChange(date, 'end')}
+                                                />
+                                            </FormControl>
+                                        </Grid>
+                                    </Grid>
+                                </Grid>
+                            </MuiPickersUtilsProvider>
+                    </DialogContentText>
+                    <DialogActions>
+                        <Grid container className={classes.marginBottom}>
+                            <Grid item xs={4} sm={4} md={6}>
+                                <Link to="/" onClick={() => setOpen(false)} className={classes.links}>
+                                    <Button startIcon={<ArrowBackIosIcon />} variant="contained" color="secondary">
+                                        Back
+                                    </Button>
+                                </Link>
+                            </Grid>
+                            <Grid align="right" item xs={8} sm={8} md={6}>
+                                <Button onClick={saveEvent} variant="contained" color="primary">
+                                    Save
                                 </Button>
-                            </Link>
+                            </Grid>
                         </Grid>
-                        <Grid align="right" item xs={8} sm={8} md={6}>
-                            <Button onClick={saveEvent} variant="contained" color="primary">
-                                Save
-                            </Button>
-                        </Grid>
-                    </Grid>
-                    <Grid container item xs={12}>Current Date : {`${year}/${month}/${day}`}</Grid>
-                    <Grid container item xs={12} sm={12} md={6}>
-                        <Grid item mt={5} xs={12} className={classes.padding}>
-                            <FormControl className={classes.selectSize}>
-                                <InputLabel htmlFor="event">Event Description</InputLabel>
-                                <Input id="event" value={event.title || ''} onChange={handleEventChange} />
-                            </FormControl>
-                        </Grid>
-                    </Grid>
-                    <Grid container item xs={12} sm={12} md={6}>
-                        <Grid item mt={5} xs={12} sm={6} className={classes.padding}>
-                            <FormControl className={classes.selectSize}>
-                                <TimePicker
-                                    ampm={false}
-                                    id="startTime"
-                                    label="Start Time"
-                                    value={event.startTime}
-                                    onChange={(date) => handleTimeChange(date, 'start')}
-                                />
-                            </FormControl>
-                        </Grid>
-                        <Grid item xs={12} sm={6} className={classes.padding}>
-                            <FormControl className={classes.selectSize}>
-                                <TimePicker
-                                    ampm={false}
-                                    id="endTime"
-                                    label="End Time"
-                                    value={event.endTime}
-                                    onChange={(date) => handleTimeChange(date, 'end')}
-                                />
-                            </FormControl>
-                        </Grid>
-                    </Grid>
-                </Grid>
-            </MuiPickersUtilsProvider>
+                    </DialogActions>
+                </DialogContent>
+            </Dialog>
         </ThemeProvider>
     )
 }
