9c8eb249a514743aeebeb0f6aa416088a1e6e656
datomegasherly <dat.omega.sherly@gmail.com>
2020-12-29 23:57:13 +0330

    call setCategory and add category data from server

--- src/components/actions/index.js
+++ src/components/actions/index.js
@@ -3,15 +3,17 @@ import axios from 'axios';
 import { configUri, updateData } from '../../helper';
 
 function effectCall(dispatch) {
-    let { setYear, setMonth, setDay } = dispatch;
+    let { setYear, setMonth, setDay, setCategory } = dispatch;
     axios.get(configUri).then(function (res) {
         let date = new Date(res.data.date);
+        let category = res.data.category;
         let y = date.getFullYear();
         let m = date.getMonth() + 1;
         let d = date.getDate();
         setYear(y);
         setMonth(m);
         setDay(d);
+        setCategory(category);
         updateData(dispatch, y, m);
     }).catch(function (error) {
         throw error;
